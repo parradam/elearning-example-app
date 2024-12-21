6899166ddb361b105c894d23db457212ffab34d3
Adam Parr <adamparr.uk@gmail.com>
2024-12-21 12:55:51 +0000

    Add ability to manage modules and contents

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -9,6 +9,11 @@ from django.contrib.auth.mixins import (
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
+from django.apps import apps
+from django.forms.models import modelform_factory
+from .models import Module, Content
+
+### Course management
 
 class OwnerMixin:
     def get_queryset(self):
@@ -46,6 +51,8 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
 
+### Module management
+
 class CourseModuleUpdateView(TemplateResponseMixin, View):
     template_name = 'courses/manage/module/formset.html'
     course = None
@@ -73,3 +80,80 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
         return self.render_to_response(
             {'course': self.course, 'formset': formset}
         )
+    
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user
+        )
+        return self.render_to_response({'module': module})
+
+### Content management
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'file', 'image', 'video']:
+            return apps.get_model(
+                app_label='courses',
+                model_name=model_name
+            )
+        return None
+    
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(
+            model, exclude=['owner', 'order', 'created', 'updated']
+        )
+        return Form(*args, **kwargs)
+    
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user
+        )
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(
+                self.model, id=id, owner=request.user
+            )
+        return super().dispatch(request, module_id, model_name, id)
+    
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response(
+            {'form': form, 'object': self.obj}
+        )
+    
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(
+            self.model,
+            instance=self.obj,
+            data=request.POST,
+            files=request.FILES
+        )
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                # New content
+                Content.objects.create(module=self.module, item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response(
+            {'form': form, 'object': self.obj}
+        )
+
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(
+            Content, id=id, module__course__owner=request.user
+        )
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
