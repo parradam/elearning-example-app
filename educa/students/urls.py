620ed02ef2cd58c45bb4793202c20e4b08699b9d
Adam Parr <adamparr.uk@gmail.com>
2024-12-23 13:53:02 +0000

    Use Redis for caching

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
     ),
     path(
         'course/<pk>/',
-        cache_page(60*15)(views.StudentCourseDetailView.as_view()),
+        cache_page(60*1)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail'
     ),
     path(
         'course/<pk>/<module_id>/',
-        cache_page(60*15)(views.StudentCourseDetailView.as_view()),
+        cache_page(60*1)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module'
     ),
 ]
