35abbe0c0f797a4627dce20c224e22e936120ec3
Adam Parr <adamparr.uk@gmail.com>
2024-12-23 17:14:36 +0000

    Annotate query with total_courses, add to serializer

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from courses.models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
+    total_courses = serializers.IntegerField()
+    
     class Meta:
         model = Subject
-        fields = ['id', 'title', 'slug']
+        fields = ['id', 'title', 'slug', 'total_courses']
