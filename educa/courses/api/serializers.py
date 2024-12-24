383a6465a599219bac25d26b8a7853fa6cb74e97
Adam Parr <adamparr.uk@gmail.com>
2024-12-24 14:16:53 +0000

    Implement nested serializer for Course and Module

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Course, Subject
+from courses.models import Module, Course, Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
@@ -26,10 +26,15 @@ class SubjectSerializer(serializers.ModelSerializer):
             'popular_courses'
         ]
 
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
 class CourseSerializer(serializers.ModelSerializer):
     # DRF does not optimise querysets, so one additional SQL query per course runs
     # Using prefetch_related('modules') can reduce the number of queries
-    modules = serializers.StringRelatedField(many=True, read_only=True)
+    modules = ModuleSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
