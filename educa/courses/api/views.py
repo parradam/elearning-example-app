51c02e0d910ffa9dff205369975e168e268bf30d
Adam Parr <adamparr.uk@gmail.com>
2024-12-24 19:24:35 +0000

    Create course enroll view with basic auth and permissions

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,11 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 # from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
@@ -23,3 +28,12 @@ class CourseViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
