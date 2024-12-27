ea92b0f9da1e1f567b3d5115f6c516cb0bb9320a
Adam Parr <adamparr.uk@gmail.com>
2024-12-27 18:35:10 +0000

    Complete synchronous WebsocketConsumer using Django Channels, allowing group chats for courses

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,14 +1,44 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
+        # Create room group using the course ID
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+
+        # Join the room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
     
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({'message': message}))
+        now = timezone.now()
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
+            }
+        )
+    
+    # Receive message from room group
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
