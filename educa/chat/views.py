69bebdfe53b3915f7b6b6f09838e8459e24ce063
Adam Parr <adamparr.uk@gmail.com>
2024-12-27 19:13:34 +0000

    Render recent messages in chat on page load

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -10,4 +10,18 @@ def course_chat_room(request, course_id):
     except Course.DoesNotExist:
         # User is not enrolled, or course does not exist
         return HttpResponseForbidden()
-    return render(request, 'chat/room.html', {'course': course})
+    
+    # Retrieve chat history
+    latest_messages = course.chat_messages.select_related(
+        'user'
+    ).order_by('-id')[:5]
+    latest_messages = reversed(latest_messages)
+
+    return render(
+        request,
+        'chat/room.html',
+        {
+            'course': course,
+            'latest_messages': latest_messages
+        }
+    )
